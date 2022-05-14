f4fed67d85064297c9bae38e65a090074ce982c1
Frank E Weiss <feweiss@gmail.com>
2022-05-13 17:45:59 -0700

    Add some notes, clean up code

--- README.md
+++ README.md
@@ -39,3 +39,18 @@ Explore ways of using log file to describe the operation of an application
 ## Links and references
 
 [IBM RSVP annotated and nestedlog file](https://www.ibm.com/docs/en/zos/2.2.0?topic=problems-example-log-file)
+
+### LAKE - Logs as Knowledge Bases
+[Other uses for log files](https://www.researchgate.net/publication/273760856_Using_Log_Files_Recorded_During_Program_Execution)
+
+coding to debugging vs logging to observation 
+
+"This  idea  can  be  expanded to include tuning logs so that logs serves as a knowledge base of program execution that con-tains precise, concise, and critical information for  software  developers to  better understand how the software runs"
+
+"logging is mainly used for debug-ging"
+
+push statistics calculation cheaply into the running program
+
+"Thereafter,  a software  program  is  obligated  to  reveal  its nontrivial  execution  details with  appropriate density in an easy-to-follow way."
+
+It looked like it was going to get into what I was looking for, but then focused on debugging.
--- apps/python/try1.py
+++ apps/python/try1.py
@@ -2,19 +2,38 @@ import re
 
 LOGFILE = "../../examples/rsvp-agent-processing.log"
 
-pattern = re.compile("(\d\d/\d\d \d\d:\d\d:\d\d) ([A-Z]+)[ ]*:(\.*)(\w+): (.+)")
-# pattern = re.compile("^(.+)$")
+# using multi line string concatenation to aid readability
+# using (?P<>...) to name match groups
+pattern = re.compile((
+    "(?P<timestamp>\d\d/\d\d \d\d:\d\d:\d\d)"
+    " "
+    "(?P<level>[A-Z]+)"
+    "[ ]*:"
+    "(?P<depth>\.*)"
+    "(?P<tag>\w+)"
+    ": "
+    "(?P<message>.+)"))
+
+# interesting tags:
+# rsvp_flow_stateMachine
+def filter(tag):
+    return tag == "reg_process"
 
 def main():
     with open(LOGFILE) as f:
         count = 0
+        tags = set()
         for line in f:
             matched = pattern.match(line)
             if matched:
                 count += 1
-                # print(matched.groupdict())
-                print(matched.groups())
+                tags.add(matched.groupdict()["tag"])
+
+                if (filter(matched.groupdict()["tag"])):
+                    # print(matched.groupdict())
+                    print(matched.groups())
         print(count)
+        print(tags)
 
 if __name__ == "__main__":
     main()
